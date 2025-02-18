bc0120251d919745116419c583bb4a8bbf75d102
AIBInnovations <aibinnovations@gmail.com>
2025-02-18 21:22:10 +0530

    Update marketResultController.js

--- controllers/marketResultController.js
+++ controllers/marketResultController.js
@@ -3,6 +3,9 @@ import moment from "moment-timezone"; // Import moment-timezone
 
 export const storeMarketResult = async (market, date, openResult, closeResult) => {
   try {
+    // Convert to Indian Standard Time (IST)
+    const istDate = moment().tz("Asia/Kolkata").format("YYYY-MM-DD");
+    
     console.log("📢 Storing Market Result for:", market.name, istDate);
 
     const openDigits = openResult.split("").map(Number);
@@ -12,9 +15,6 @@ export const storeMarketResult = async (market, date, openResult, closeResult) =
     const closeSingleDigit = closeDigits.reduce((sum, digit) => sum + digit, 0) % 10;
 
     const jodiResult = `${openSingleDigit}${closeSingleDigit}`;
-
-    // Convert to Indian Standard Time (IST)
-    const istDate = moment().tz("Asia/Kolkata").format("YYYY-MM-DD");
 
     // Store result in MarketResult collection
     await MarketResult.create({
